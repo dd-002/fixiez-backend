789878bb02a959612de62bc064845fac9f53e8d6
Dipayan Das <d.dipayan2002@gmail.com>
2026-01-29 16:35:00 +0530

    added routes index file

---
+++ src/routes/index.js
@@ -0,0 +1,32 @@
+//Exports all the routes
+import { Router } from "express";
+// import authRoutes from "./authRoutes.js"; //for authorisation
+// import productAdminRoutes from "./productAdmin.js";
+// import productUserRoutes from "./productUser.js";
+// import paymentRoutes from "./paymentRoutes.js";
+// import orderRoutes from "./order.js";
+// import commitmentRoutes from "./commitmentRoutes.js";
+// import orderAdminRoutes from "./orderAdmin.js";
+// import userAdminRoutes from "./userAdminRoutes.js";
+// import testRoutes from "./test.js";
+// import commitmentAdminRoutes from "./commitmentRoutesAdmin.js";
+// import adminAddressRoutes from "./adminAddressRoutes.js";
+
+const router = Router();
+
+// router.use("/auth", authRoutes);
+// router.use("/productUser", productUserRoutes);
+// router.use("/payments", paymentRoutes);
+// router.use("/order", orderRoutes);
+// router.use("/commitment", commitmentRoutes);
+
+// //admin routes
+// router.use("/product-admin", productAdminRoutes);
+// router.use("/admin/order", orderAdminRoutes);
+// router.use("/admin/user-admin", userAdminRoutes);
+// router.use("/admin/commitment-admin", commitmentAdminRoutes);
+// router.use("/admin/address-admin", adminAddressRoutes);
+
+// router.use("/test", testRoutes);
+
+export default router;
