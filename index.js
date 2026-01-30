d50ea316b743310e3c7d42599200d35cf0ba810e
Dipayan Das <d.dipayan2002@gmail.com>
2026-01-30 12:15:28 +0530

    added session support for authentication

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ const app = express();
 dotenv.config()
 
 const port = process.env.PORT || 8000;
+
 //basic middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+//auth middlewares
+app.use(session({
+  secret: process.env.SESSION_SECRET, // A strong secret key
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+  cookie: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 4 * 7 * 24 * 60 * 60 * 1000 // 4 weeks
+  }
+}));
 
 
 //routing
