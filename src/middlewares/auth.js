6c6601cef1f0079a5f8c5aadf3deae3bded33b19
Dipayan Das <d.dipayan2002@gmail.com>
2026-03-02 00:18:44 +0530

    Bug fix in RBAC

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -3,16 +3,16 @@ const isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.status(401).json({ message: "Unauthorized. Please log in." });
+  return res.status(401).json({ message: "Unauthorized. Please log in" });
 };
 
 // Check if the user has a specific role
-const authorize = (...roles) => {
+const authorize = (roles) => {
   return (req, res, next) => {
     if (!req.user) {
       return res.status(401).json({ message: "Unauthorized" });
     }
-
+    console.log(req.user.role, roles)
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({ message: "Forbidden: Insufficient permissions" });
     }
